e2f8e2602193d66c5057271bd2c8499665e197e9
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2024-10-11 16:26:44 +0300

    Convert mp4 to webm

--- frontend/src/components/Courses/CourseContent.jsx
+++ frontend/src/components/Courses/CourseContent.jsx
@@ -1,8 +1,8 @@
 import MarkdownPreview from "@uiw/react-markdown-preview";
-import video1 from "../../assets/Videos/lec1-1.mp4";
-import video2 from "../../assets/Videos/lec1-2.mp4";
-import video3 from "../../assets/Videos/lec1-3.mp4";
-import video4 from "../../assets/Videos/lec1-4.mp4";
+import video1 from "../../assets/Videos/lec1-1.webm";
+import video2 from "../../assets/Videos/lec1-2.webm";
+import video3 from "../../assets/Videos/lec1-3.webm";
+import video4 from "../../assets/Videos/lec1-4.webm";
 import lec1 from "../../assets/Lectures/lec1.md?raw";
 
 const CourseContent = ({ id }) => {
