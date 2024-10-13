b26b35892a54af2bc652ad7be45018f33462c01f
hussein-saad <hussein.saad2021@gmail.com>
2024-10-14 00:59:24 +0300

    Remove cors middleware

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(cookieParser());
