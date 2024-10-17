22d897cfa0c497d018325e6871c4339209cb509d
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2024-10-17 17:28:28 +0300

    Remove unused headers file and update Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import string from "vite-plugin-string";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    string({
-      include: "**/*.md",
-    }),
-  ],
+  plugins: [react()],
 });
